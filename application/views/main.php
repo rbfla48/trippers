7796e87f57b2469d431d441882dc7e741a8aeede
kyurim <rbfla48@naver.com>
2024-08-04 17:19:51 +0900

    헤더,푸터 양식변경
    공통으로쓰면 위치 달라짐

--- application/views/main.php
+++ application/views/main.php
@@ -1,4 +1,6 @@
 <?php include_once("layout/header.php")?>
+<link rel="stylesheet" href="assets/css/globals.css" />
+<link rel="stylesheet" href="assets/css/style.css" />
 <body>
   <div class="PC">
     <div class="div">
@@ -102,6 +104,24 @@
           <div class="text-wrapper-14">Menu</div>
         </div>
       </div>
+      <!--하단기업정보-->
+      <div class="view-6">
+        <img class="b" src="https://c.animaapp.com/IWpl16yO/img/-----b-1@2x.png" />
+        <p class="element-11">
+          <span class="span">트리퍼<br /></span>
+          <span class="text-wrapper-15">대표&nbsp;&nbsp;이진우 | 사업자 등록번호 732-17-02122 | 통신판매업 신고번호 2023-경남 남해-105호<br />경남
+            남해군 삼동면 동부대로 1876번길 15<br /></span>
+        </p>
+        <div class="ABOUT-NEWSLETTER">
+          ABOUT&nbsp;&nbsp;|&nbsp;&nbsp; NEWSLETTER&nbsp;&nbsp;|&nbsp;&nbsp; INSTAGRAM&nbsp;&nbsp;|&nbsp;&nbsp;YOUTUBE
+        </div>
+        <div class="rectangle-5"></div>
+      </div>
+      <!--하단기업정보/-->
+      <img class="user" src="https://c.animaapp.com/IWpl16yO/img/user.svg" />
+      <img class="search" src="https://c.animaapp.com/IWpl16yO/img/search.svg" />
+    </div>
+  </div>
 <?php include_once("layout/footer.php")?>
 </body>
 
