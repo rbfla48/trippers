ae1c3f0b12387fa23e63d699ca55c4c3cf0340b8
kyurim <rbfla48@naver.com>
2024-08-04 16:24:04 +0900

    프론트 헤더,푸터 공통파일로 변환
    views/layout/header.php
    views/layout/footer.php

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,4 +1,51 @@
-</main>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>    
-</body>
-</html>
+    <div class="view-6">
+        <img class="b" src="https://c.animaapp.com/IWpl16yO/img/-----b-1@2x.png" />
+        <p class="element-11">
+          <span class="span">트리퍼<br /></span>
+          <span class="text-wrapper-15">대표&nbsp;&nbsp;이진우 | 사업자 등록번호 732-17-02122 | 통신판매업 신고번호 2023-경남 남해-105호<br />경남
+            남해군 삼동면 동부대로 1876번길 15<br /></span>
+        </p>
+        <div class="ABOUT-NEWSLETTER">
+          ABOUT&nbsp;&nbsp;|&nbsp;&nbsp; NEWSLETTER&nbsp;&nbsp;|&nbsp;&nbsp; INSTAGRAM&nbsp;&nbsp;|&nbsp;&nbsp;YOUTUBE
+        </div>
+        <div class="rectangle-5"></div>
+      </div>
+      <img class="user" src="https://c.animaapp.com/IWpl16yO/img/user.svg" />
+      <img class="search" src="https://c.animaapp.com/IWpl16yO/img/search.svg" />
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  <script>
+    const bannerSwiper = new Swiper('.banner-swiper', {
+      // 슬라이더 방향
+      direction: 'horizontal', // 일반적으로 가로 방향 사용
+      loop: true,
+
+      // 페이지 네비게이션
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        bulletClass: 'swiper-pagination-bullet',
+        bulletActiveClass: 'swiper-pagination-bullet-active',
+      },
+
+      // 네비게이션 버튼
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      // 스크롤바 (선택 사항)
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+
+    const articleSwiper = new Swiper('.article-slide', {
+      slidesPerView: 4, // 한 슬라이드에 4개의 타일 표시
+      spaceBetween: 100, // 슬라이드 간 간격
+      direction: 'horizontal', // 일반적으로 가로 방향 사용
+      loop: true, // 무한 반복
+    });
+  </script>
--- application/views/main.php
+++ application/views/main.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>trippers</title>
-  <!--슬라이드-->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-  <!--슬라이드/-->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="stylesheet" href="assets/css/globals.css" />
-  <link rel="stylesheet" href="assets/css/style.css" />
-</head>
-
+<?php include_once("layout/header.php")?>
 <body>
   <div class="PC">
     <div class="div">
@@ -117,57 +102,7 @@
           <div class="text-wrapper-14">Menu</div>
         </div>
       </div>
-      <div class="view-6">
-        <img class="b" src="https://c.animaapp.com/IWpl16yO/img/-----b-1@2x.png" />
-        <p class="element-11">
-          <span class="span">트리퍼<br /></span>
-          <span class="text-wrapper-15">대표&nbsp;&nbsp;이진우 | 사업자 등록번호 732-17-02122 | 통신판매업 신고번호 2023-경남 남해-105호<br />경남
-            남해군 삼동면 동부대로 1876번길 15<br /></span>
-        </p>
-        <div class="ABOUT-NEWSLETTER">
-          ABOUT&nbsp;&nbsp;|&nbsp;&nbsp; NEWSLETTER&nbsp;&nbsp;|&nbsp;&nbsp; INSTAGRAM&nbsp;&nbsp;|&nbsp;&nbsp;YOUTUBE
-        </div>
-        <div class="rectangle-5"></div>
-      </div>
-      <img class="user" src="https://c.animaapp.com/IWpl16yO/img/user.svg" />
-      <img class="search" src="https://c.animaapp.com/IWpl16yO/img/search.svg" />
-    </div>
-  </div>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-  <script>
-    const bannerSwiper = new Swiper('.banner-swiper', {
-      // 슬라이더 방향
-      direction: 'horizontal', // 일반적으로 가로 방향 사용
-      loop: true,
-
-      // 페이지 네비게이션
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        bulletClass: 'swiper-pagination-bullet',
-        bulletActiveClass: 'swiper-pagination-bullet-active',
-      },
-
-      // 네비게이션 버튼
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-
-      // 스크롤바 (선택 사항)
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
-    });
-
-    const articleSwiper = new Swiper('.article-slide', {
-      slidesPerView: 4, // 한 슬라이드에 4개의 타일 표시
-      spaceBetween: 40, // 슬라이드 간 간격
-      direction: 'horizontal', // 일반적으로 가로 방향 사용
-      loop: true, // 무한 반복
-    });
-  </script>
+<?php include_once("layout/footer.php")?>
 </body>
 
 </html>
