230fa405cbe1b2883ebd3c561164acbe237bc181
김경묵 <wiz0420@naver.com>
2025-08-05 16:40:31 +0900

    이미지 수정 및 오류 수정

--- application/views/event_calendar.php
+++ application/views/event_calendar.php
@@ -588,9 +588,6 @@
 						<img src="assets/img/namhae04.svg" onclick="window.open('https://walla.my/survey/y94fnZEQgIZuabehogjq')">
 					</div>
 				</div>
-				<div class="w-100 text-center" style="margin-top: 5%;">
-					<img src="assets/img/shortcut.svg" onclick="window.open('https://walla.my/survey/y94fnZEQgIZuabehogjq')">
-				</div>
 			</section>
 			<section id="mo-event-banner" class="swiper mySwiper media-mt-0 px-4" style="max-width: 1420px; padding: 40px 0; margin-top: 5%;">
 				<div class="swiper-pagination"></div>
